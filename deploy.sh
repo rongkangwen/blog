a7764e5d819fc084b363fb3b3de80604980ac4ed
rongkangwen <rongkangwen@outlook.com>
2021-05-26 10:29:33 +0800

    修改脚本

--- deploy.sh
+++ deploy.sh
@@ -10,14 +10,25 @@ yarn build
 cd docs/.vuepress/dist
 
 # 如果是发布到自定义域名
-# echo 'www.example.com' > CNAME
+echo 'www.rongkangwen.cn' > CNAME
+echo 'rongkangwen.github.io' >> README.md
+
+if [ -z "$GITHUB_TOKEN" ]; then
+  msg='deploy'
+  githubUrl=git@github.com:rongkangwen/rongkangwen.github.io.git
+else
+  msg='来自github actions的自动部署'
+  githubUrl=https://rongkangwen:${GITHUB_TOKEN}@github.com:rongkangwen/rongkangwen.github.io.git
+  git config --global user.name "rongkangwen"
+  git config --global user.email "rongkangwen@outlook.com"
+fi
 
 git init
 git add -A
-git commit -m 'deploy'
+git commit -m "${msg}"
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:rongkangwen/rongkangwen.github.io.git master
+git push -f $githubUrl master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
